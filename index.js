9e78aa19824f641adcf817b24b906fc9cbd00afc
Kirianich <36043696+Kirianich@users.noreply.github.com>
2025-03-28 03:49:15 +0600

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ client.commands = new Collection();
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
     const command = require(`./commands/${file}`);
     client.commands.set(command.data.name, command);
 }
