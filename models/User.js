18b6354fd56be877dd40cdfcacbd02aaeee49ac2
Kirianich <36043696+Kirianich@users.noreply.github.com>
2025-03-29 03:08:44 +0600

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,10 +1,9 @@
- const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-    discordId: String,
-    minecraftUUID: String,
-    ign: String,
-    guildId: String
+const VerifiedUserSchema = new mongoose.Schema({
+    discordId: { type: String, required: true, unique: true },
+    username: { type: String, required: true },
+    guildId: { type: String, required: true }
 });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('VerifiedUser', VerifiedUserSchema);
