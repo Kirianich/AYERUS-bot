d84326d9adada72ada486fd096d8e2e0687faca3
Kirianich <36043696+Kirianich@users.noreply.github.com>
2025-03-31 14:27:18 +0300

    Update setupVerification.js

--- commands/setupVerification.js
+++ commands/setupVerification.js
@@ -1,5 +1,4 @@
 const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, PermissionFlagsBits } = require('discord.js');
-const GuildSettings = require('../../models/GuildSettings');
 
 module.exports = {
     data: new SlashCommandBuilder()
