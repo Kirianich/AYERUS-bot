89881902b0deca8a52affd2c0214b015c2a07eab
Kirianich <36043696+Kirianich@users.noreply.github.com>
2025-03-31 17:24:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -91,6 +91,6 @@ client.once('ready', async () => {
     console.log(`✅ Logged in as ${client.user.tag}`);
     await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
     console.log("✅ Connected to MongoDB");
-}
+});
 
 client.login(process.env.TOKEN); 
