338b8ef0d6e691e6e77df64bb4bfa09e65b4ca5f
Kirianich <36043696+Kirianich@users.noreply.github.com>
2025-04-01 14:16:30 +0300

    Update verify_submit.js

--- interactions/modals/verify_submit.js
+++ interactions/modals/verify_submit.js
@@ -8,13 +8,13 @@ require('dotenv').config();
 module.exports = {
     customId: 'verification_modal',
     async execute(interaction) {
-        try {
+        
             const username = interaction.fields.getTextInputValue('minecraft_username');
             const discordId = interaction.user.id;
             console.log("🔍 Received interaction for:", interaction.customId);      
             await interaction.deferReply({ ephemeral: true });
             console.log("✅ Interaction deferred");  
-
+        try {
             // Check if user is already verified
             const existingUser = await User.findOne({ discordId });
             if (existingUser) {
