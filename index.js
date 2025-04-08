683941917a21620ce1eafad57511c2c822b4b1b8
Kirianich <36043696+Kirianich@users.noreply.github.com>
2025-04-08 18:25:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -39,6 +39,10 @@ client.on(Events.InteractionCreate, async interaction => {
     }
 });
 
+process.on('unhandledRejection', (error) => {
+  console.error('Unhandled promise rejection:', error);
+});
+
 client.once('ready', async () => {
     console.log(`✅ Logged in as ${client.user.tag}`);
     await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
