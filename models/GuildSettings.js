6524db36b6b6f9b8c9bb584286a4cbcff5c023d8
Kirianich <36043696+Kirianich@users.noreply.github.com>
2025-04-10 01:53:57 +0600

    Update GuildSettings.js

--- models/GuildSettings.js
+++ models/GuildSettings.js
@@ -4,6 +4,23 @@ const GuildSettingsSchema = new mongoose.Schema({
     guildId: { type: String, required: true, unique: true },
     verifiedRole: { type: String, default: null },
     unverifiedRole: { type: String, default: null },
+    guestRole: { type: String, default: null }, // Applies if user is NOT in any linked guild
+
+    linkedGuilds: [
+    {
+      hypixelGuildId: String,
+      hypixelGuildName: String,
+      roles: {
+        guildMemberRole: String,    // Applies if user is in the Hypixel guild          
+        rankRoles: {
+          rank1: String,            // For top rank (excluding GM)
+          rank2: String,
+          rank3: String,
+          rank4: String
+        }
+      }
+    }
+  ]
 });
 
 module.exports = mongoose.model('GuildSettings', GuildSettingsSchema);
