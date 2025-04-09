d2d562a6d3ec4a76b188655fabe7ad5712f52678
Kirianich <36043696+Kirianich@users.noreply.github.com>
2025-04-09 17:51:29 +0300

    Update roleSelectorVerified.js

--- interactions/selectMenus/settings/roleSelectorVerified.js
+++ interactions/selectMenus/settings/roleSelectorVerified.js
@@ -1,1 +1,23 @@
+const GuildSettings = require('../../../models/GuildSettings');
+const { buildRoleSettingsMessage } = require('../../../utils/settingsUI');
 
+module.exports = {
+    customId: 'settings_select_verified_role',
+    async execute(interaction) {
+        const selectedRoleId = interaction.values[0];
+        const guildId = interaction.guild.id;
+
+        await GuildSettings.findOneAndUpdate(
+            { guildId },
+            { verifiedRole: selectedRoleId },
+            { upsert: true }
+        );
+
+        const { embed, components } = await buildRoleSettingsMessage(interaction.guild);
+        await interaction.update({
+            content: '✅ Роль для верифицированных пользователей успешно обновлена!',
+            embeds: [embed],
+            components
+        });
+    }
+};
