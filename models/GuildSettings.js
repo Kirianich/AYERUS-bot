5046a822845fff825d541312e0328e4bf3da2697
Kirianich <36043696+Kirianich@users.noreply.github.com>
2025-04-09 15:13:09 +0300

    Update GuildSettings.js

--- models/GuildSettings.js
+++ models/GuildSettings.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const GuildSettingsSchema = new mongoose.Schema({
     guildId: { type: String, required: true, unique: true },
-    verifiedRole: { type: String }
+    verifiedRole: { type: String, default: null },
+    unverifiedRole: { type: String, default: null },
 });
 
 module.exports = mongoose.model('GuildSettings', GuildSettingsSchema);
