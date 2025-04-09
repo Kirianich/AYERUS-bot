8332f59643a60827259660df51675cb605e3c4a8
Kirianich <36043696+Kirianich@users.noreply.github.com>
2025-04-09 18:08:32 +0300

    Update roleSelectorUnverified.js

--- interactions/selectMenus/settings/roleSelectorUnverified.js
+++ interactions/selectMenus/settings/roleSelectorUnverified.js
@@ -1,11 +1,15 @@
 const GuildSettings = require('../../../models/GuildSettings');
 const { buildRoleSettingsMessage } = require('../../../utils/settingsUI');
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 
 module.exports = {
     customId: 'settings_select_unverified_role',
     async execute(interaction) {
         const selectedRoleId = interaction.values[0];
         const guildId = interaction.guild.id;
+
+        console.log("Selected Role:", selectedRoleId);
+        console.log("Guild ID:", guildId);
 
         await GuildSettings.findOneAndUpdate(
             { guildId },
@@ -14,10 +18,18 @@ module.exports = {
         );
 
         const { embed, components } = await buildRoleSettingsMessage(interaction.guild);
+
+        const backRow = new ActionRowBuilder().addComponents(
+            new ButtonBuilder()
+                .setCustomId('settings_go_back:roles')
+                .setLabel('🔙 Назад')
+                .setStyle(ButtonStyle.Danger)
+        );
+
         await interaction.update({
             content: '✅ Роль для неверифицированных пользователей успешно обновлена!',
             embeds: [embed],
-            components
+            components: [...components, backRow]
         });
     }
 };
