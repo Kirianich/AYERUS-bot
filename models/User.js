0f26a0de27132b83aa544383c734815e062c4652
Kirianich <36043696+Kirianich@users.noreply.github.com>
2025-04-09 12:47:07 +0300

    Update User.js

--- models/User.js
+++ models/User.js
@@ -3,10 +3,11 @@ const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
     discordId: { type: String, required: true, unique: true },
     username: { type: String, required: true },
+    hypixelUuid: {type: String, required: true },
     guildId: { type: String, required: true }, //Discord Server
     hypixelGuild: { type: String, default: "None" }, // Hypixel Guild Name
-    guildRank: { type: String, default: "Member" }, // Rank inside the Hypixel Guild
-    rank: { type: String, default: "Default" }, // Hypixel Rank
+    hypixelGuildRank: { type: String, default: "Member" }, // Rank inside the Hypixel Guild
+    hypixelRank: { type: String, default: "Default" }, // Hypixel Rank
     skyblockLevel: { type: Number, default: 0 }, // Skyblock Level
     skyblockSkills: { type: Object, default: {} } // Skyblock Skill Levels
 });
