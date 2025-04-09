0565067a2f107d9b88b576f83e1d762744344b68
Kirianich <36043696+Kirianich@users.noreply.github.com>
2025-04-09 17:51:39 +0300

    Update roleSelectorUnverified.js

--- interactions/selectMenus/settings/roleSelectorUnverified.js
+++ interactions/selectMenus/settings/roleSelectorUnverified.js
@@ -1,1 +1,23 @@
+const GuildSettings = require('../../../models/GuildSettings');
+const { buildRoleSettingsMessage } = require('../../../utils/settingsUI');
 
+module.exports = {
+    customId: 'settings_select_unverified_role',
+    async execute(interaction) {
+        const selectedRoleId = interaction.values[0];
+        const guildId = interaction.guild.id;
+
+        await GuildSettings.findOneAndUpdate(
+            { guildId },
+            { unverifiedRole: selectedRoleId },
+            { upsert: true }
+        );
+
+        const { embed, components } = await buildRoleSettingsMessage(interaction.guild);
+        await interaction.update({
+            content: '✅ Роль для неверифицированных пользователей успешно обновлена!',
+            embeds: [embed],
+            components
+        });
+    }
+};
