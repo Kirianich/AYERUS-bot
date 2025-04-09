d18c8631fbb3e8de8f1d4865ef7dea15ecb87058
Kirianich <36043696+Kirianich@users.noreply.github.com>
2025-04-09 16:03:00 +0300

    Update settings.js

--- commands/settings.js
+++ commands/settings.js
@@ -1,1 +1,17 @@
+const { SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder } = require('discord.js');
+const { buildInitialSettingsMessage } = require('../utils/settingsUI');
 
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('settings')
+        .setDescription('Configure bot settings for this server'),
+    async execute(interaction) {
+        const { embed, components } = await buildInitialSettingsMessage(interaction.guild);
+
+        await interaction.reply({
+            embeds: [embed],
+            components,
+            ephemeral: false
+        });
+    }
+};
