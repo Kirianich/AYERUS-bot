a1b54f6f63387ffddec05a04eab250d4990be665
Kirianich <36043696+Kirianich@users.noreply.github.com>
2025-04-09 17:46:45 +0300

    Update setUnverifiedRole.js

--- interactions/buttons/settings/setUnverifiedRole.js
+++ interactions/buttons/settings/setUnverifiedRole.js
@@ -1,1 +1,27 @@
+const { ActionRowBuilder, RoleSelectMenuBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 
+module.exports = {
+    customId: 'settings_set_unverified_role',
+    async execute(interaction) {
+        const menu = new RoleSelectMenuBuilder()
+            .setCustomId('settings_select_unverified_role')
+            .setPlaceholder('Выберите роль для неверифицированных пользователей')
+            .setMinValues(1)
+            .setMaxValues(1);
+
+        const row = new ActionRowBuilder().addComponents(menu);
+
+        const backRow = new ActionRowBuilder().addComponents(
+            new ButtonBuilder()
+                .setCustomId('settings_go_back:roles')
+                .setLabel('🔙 Назад')
+                .setStyle(ButtonStyle.Danger)
+        );
+
+        await interaction.update({
+            content: '📌 Пожалуйста, выберите новую роль для **неверифицированных** пользователей:',
+            embeds: [],
+            components: [row, backRow]
+        });
+    }
+};
