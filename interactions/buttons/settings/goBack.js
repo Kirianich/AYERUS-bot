b5157a2ee52a87fdeab9210f4a7b1505bc8ccae1
Kirianich <36043696+Kirianich@users.noreply.github.com>
2025-04-09 16:41:58 +0300

    Update goBack.js

--- interactions/buttons/settings/goBack.js
+++ interactions/buttons/settings/goBack.js
@@ -1,1 +1,9 @@
+const { buildInitialSettingsMessage } = require('../../../utils/settingsUI');
 
+module.exports = {
+    customId: 'settings_go_back_main',
+    async execute(interaction) {
+        const { embed, components } = await buildInitialSettingsMessage(interaction.guild);
+        await interaction.update({ embeds: [embed], components });
+    }
+};
