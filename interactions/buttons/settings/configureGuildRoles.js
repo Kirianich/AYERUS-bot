d7843488afdd40041fc22552690b886ca51ccc1e
Kirianich <36043696+Kirianich@users.noreply.github.com>
2025-04-10 14:32:52 +0300

    Update configureGuildRoles.js

--- interactions/buttons/settings/configureGuildRoles.js
+++ interactions/buttons/settings/configureGuildRoles.js
@@ -3,7 +3,7 @@ const { ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder } = require('
 const { buildInitialSettingsMessage, buildGuildSelectPanel } = require('../../../utils/settingsUI');
 
 module.exports = {
-  customId: 'settings_configure_roles',
+  customId: 'settings_configure_guild_ranks',
   const { embed, components } = await buildGuildSelectPanel(settings);
   await interaction.update({ embeds: [embed], components });
 
