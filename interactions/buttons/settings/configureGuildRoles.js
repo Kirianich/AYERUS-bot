d20841929de27a0528cec76aac553ad059f1ff13
Kirianich <36043696+Kirianich@users.noreply.github.com>
2025-04-10 14:31:27 +0300

    Update configureGuildRoles.js

--- interactions/buttons/settings/configureGuildRoles.js
+++ interactions/buttons/settings/configureGuildRoles.js
@@ -2,7 +2,8 @@ const GuildSettings = require('../../../models/GuildSettings');
 const { ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder } = require('discord.js');
 const { buildInitialSettingsMessage, buildGuildSelectPanel } = require('../../../utils/settingsUI');
 
-
+module.exports = {
 const { embed, components } = await buildGuildSelectPanel(settings);
 await interaction.update({ embeds: [embed], components });
 
+};
