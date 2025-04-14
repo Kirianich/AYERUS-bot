f87a0b524f358d2398107cc38cce402cc08e1dca
Kirianich <36043696+Kirianich@users.noreply.github.com>
2025-04-14 16:22:48 +0300

    Add files via upload

---
+++ interactions/buttons/settings/selectNetworkRankRole.js
@@ -0,0 +1,32 @@
+const { ActionRowBuilder, RoleSelectMenuBuilder, EmbedBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+
+module.exports = {
+  customId: /^configure_network_rank:.+/,
+  async execute(interaction) {
+    const rank = interaction.customId.split(':')[1];
+
+    const embed = new EmbedBuilder()
+      .setTitle(`🎖 Настройка роли для ${rank.replace(/_/g, ' ')}`)
+      .setDescription('Выберите роль, которую необходимо присвоить пользователям с этим Hypixel рангом.')
+      .setColor(0x5865F2);
+
+    const roleSelect = new RoleSelectMenuBuilder()
+      .setCustomId(`commit_network_rank:${rank}`)
+      .setPlaceholder('Выберите роль…')
+      .setMinValues(1)
+      .setMaxValues(1);
+
+    const goBackButton = new ButtonBuilder()
+      .setCustomId('settings_configure:network_ranks')
+      .setLabel('⬅️ Назад')
+      .setStyle(ButtonStyle.Secondary);
+
+    const row1 = new ActionRowBuilder().addComponents(roleSelect);
+    const row2 = new ActionRowBuilder().addComponents(goBackButton);
+
+    await interaction.update({
+      embeds: [embed],
+      components: [row1, row2]
+    });
+  }
+};
