86efdf76e058ffa7b94f4fc4efd2145509186416
Kirianich <36043696+Kirianich@users.noreply.github.com>
2025-04-14 16:17:00 +0300

    Update configureNetworkRanks.js

--- interactions/buttons/settings/configureNetworkRanks.js
+++ interactions/buttons/settings/configureNetworkRanks.js
@@ -1,21 +1,35 @@
 const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+const GuildSettings = require('../../../models/GuildSettings');
 
 module.exports = {
   customId: 'settings_configure:network_ranks',
   async execute(interaction) {
+    const guildId = interaction.guild.id;
+    const settings = await GuildSettings.findOne({ discordGuildId: guildId }) || await GuildSettings.create({ discordGuildId: guildId });
+
     const embed = new EmbedBuilder()
       .setTitle('🎖 Настройка Hypixel Rank ролей')
       .setDescription('Выберите Discord-роль для каждого Hypixel ранга:')
       .setColor(0x5865F2);
 
     const ranks = ['DEFAULT', 'VIP', 'VIP_PLUS', 'MVP', 'MVP_PLUS', 'MVP_PLUS_PLUS', 'YOUTUBE', 'ADMIN'];
+    const roleLines = [];
 
-    const buttons = ranks.map(rank =>
-      new ButtonBuilder()
+    const buttons = ranks.map(rank => {
+      const roleId = settings.networkRankRoles?.get(rank) || null;
+      if (roleId) {
+        roleLines.push(`**${rank.replace(/_/g, ' ')}** ➝ <@&${roleId}>`);
+      } else {
+        roleLines.push(`**${rank.replace(/_/g, ' ')}** ➝ _Не задано_`);
+      }
+
+      return new ButtonBuilder()
         .setCustomId(`configure_network_rank:${rank}`)
         .setLabel(rank.replace(/_/g, ' '))
-        .setStyle(ButtonStyle.Secondary)
-    );
+        .setStyle(roleId ? ButtonStyle.Success : ButtonStyle.Secondary);
+    });
+
+    embed.addFields({ name: 'Текущие настройки:', value: roleLines.join('\n') });
 
     const backButton = new ButtonBuilder()
       .setCustomId('settings_go_back:main')
