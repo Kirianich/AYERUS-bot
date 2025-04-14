5084054634df3c5476a08dee163aa001029a5234
Kirianich <36043696+Kirianich@users.noreply.github.com>
2025-04-14 16:27:43 +0300

    Update selectNetworkRankRole.js

--- interactions/buttons/settings/selectNetworkRankRole.js
+++ interactions/buttons/settings/selectNetworkRankRole.js
@@ -1,14 +1,24 @@
 const { ActionRowBuilder, RoleSelectMenuBuilder, EmbedBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+const GuildSettings = require('../../../models/GuildSettings');
 
 module.exports = {
   customId: /^configure_network_rank:.+/,
   async execute(interaction) {
     const rank = interaction.customId.split(':')[1];
+    const guildId = interaction.guild.id;
+
+    const settings = await GuildSettings.findOne({ discordGuildId: guildId }) || await GuildSettings.create({ discordGuildId: guildId });
+    const currentRoleId = settings.networkRankRoles?.get(rank);
 
     const embed = new EmbedBuilder()
       .setTitle(`🎖 Настройка роли для ${rank.replace(/_/g, ' ')}`)
       .setDescription('Выберите роль, которую необходимо присвоить пользователям с этим Hypixel рангом.')
       .setColor(0x5865F2);
+
+    embed.addFields({
+      name: 'Текущая роль:',
+      value: currentRoleId ? `<@&${currentRoleId}>` : '_Не задано_'
+    });
 
     const roleSelect = new RoleSelectMenuBuilder()
       .setCustomId(`commit_network_rank:${rank}`)
