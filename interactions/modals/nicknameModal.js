7247409285ae6c683f9b354434aa1d9d916e5f55
Kirianich <36043696+Kirianich@users.noreply.github.com>
2025-04-14 17:44:04 +0300

    Add files via upload

---
+++ interactions/modals/nicknameModal.js
@@ -0,0 +1,20 @@
+const GuildSettings = require('../../../models/GuildSettings');
+
+module.exports = {
+  customId: 'nickname_modal',
+  async execute(interaction) {
+    const newFormat = interaction.fields.getTextInputValue('nickname_format_input');
+    const guildId = interaction.guild.id;
+
+    await GuildSettings.findOneAndUpdate(
+      { discordGuildId: guildId },
+      { nicknameFormat: newFormat },
+      { upsert: true }
+    );
+
+    await interaction.reply({
+      content: `✅ Формат ника обновлён на: \`${newFormat}\``,
+      ephemeral: true
+    });
+  }
+};
