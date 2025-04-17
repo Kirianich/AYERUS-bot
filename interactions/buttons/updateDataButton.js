ad9476c95bd88f4c4dc4a8200fe525a18658abd5
Kirianich <36043696+Kirianich@users.noreply.github.com>
2025-04-17 12:41:21 +0300

    Update updateDataButton.js

--- interactions/buttons/updateDataButton.js
+++ interactions/buttons/updateDataButton.js
@@ -1,15 +1,20 @@
 
 const { ButtonBuilder, ButtonStyle, ActionRowBuilder, EmbedBuilder } = require('discord.js');
+const { Updater } = require('../../utils/updater');
 
 module.exports = {
   customId: 'update_data_button',
   async execute(interaction) {
-    const updater = require('../../utils/updater');
     try {
-      await updater(interaction);
+      const updater = new Updater(interaction);
+      await updater.process();
     } catch (error) {
       console.error('Update Error:', error);
-      await interaction.reply({ content: '❌ Ошибка при обновлении данных. Попробуйте позже.', ephemeral: true });
+      await interaction.reply({
+        content: '❌ Ошибка при обновлении данных. Попробуйте позже.',
+        ephemeral: true
+      });
     }
   },
 };
+
