4cea95a8361bd7cbbc61f6614fd879b64ecccbd1
Kirianich <36043696+Kirianich@users.noreply.github.com>
2025-04-17 17:12:17 +0300

    Update skyblockRoles.js

--- utils/skyblockRoles.js
+++ utils/skyblockRoles.js
@@ -1,18 +1,18 @@
 // utils/skyblockRoles.js
-
 const skyblockLevelRoles = [
-  { min: 1, max: 39, roleId: 'ROLE_ID_1' },
-  { min: 40, max: 79, roleId: 'ROLE_ID_2' },
-  { min: 80, max: 119, roleId: 'ROLE_ID_3' },
-  { min: 120, max: 159, roleId: 'ROLE_ID_4' },
-  { min: 160, max: 199, roleId: 'ROLE_ID_5' },
-  { min: 200, max: 239, roleId: 'ROLE_ID_6' },
-  { min: 240, max: 279, roleId: 'ROLE_ID_7' },
-  { min: 280, max: 319, roleId: 'ROLE_ID_8' },
-  { min: 320, max: 359, roleId: 'ROLE_ID_9' },
-  { min: 360, max: 399, roleId: 'ROLE_ID_10' },
-  { min: 400, max: 479, roleId: 'ROLE_ID_11' },
-  { min: 480, max: 500, roleId: 'ROLE_ID_12' }
+  { min: 1, max: 39, roleId: '1362415393926283426' },
+  { min: 40, max: 79, roleId: '1362415669362167949' },
+  { min: 80, max: 119, roleId: '1362415913151889418' },
+  { min: 120, max: 159, roleId: '1362416163639791769' },
+  { min: 160, max: 199, roleId: '1362416278404469098' },
+  { min: 200, max: 239, roleId: '1362416415940018267' },
+  { min: 240, max: 279, roleId: '1362416531820118289' },
+  { min: 280, max: 319, roleId: '1362416718940733620' },
+  { min: 320, max: 359, roleId: '1362416876139053118' },
+  { min: 360, max: 399, roleId: '1362417029675745281' },
+  { min: 400, max: 439, roleId: '1362417128388694166' },
+  { min: 440, max: 479, roleId: '1362417277290414080' },
+  { min: 480, max: 500, roleId: '1362417390058733589' }
 ];
 
 async function applySkyblockLevelRole(member, sbLevel) {
